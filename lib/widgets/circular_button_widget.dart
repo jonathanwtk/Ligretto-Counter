7f1dfb9627774b765743b6b05561d69022ccd085
johnywtk <33962161+johnywtk@users.noreply.github.com>
2022-12-19 17:47:38 +0100

    Button dunkler bei tippen

--- lib/widgets/circular_button_widget.dart
+++ lib/widgets/circular_button_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ligretto_counter/constants.dart';
 
-class CircularButton extends StatelessWidget {
+class CircularButton extends StatefulWidget {
   final IconData icon;
   final Function() onTap;
   final bool hasShadow;
@@ -20,20 +20,52 @@ class CircularButton extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CircularButton> createState() => _CircularButtonState();
+}
+
+class _CircularButtonState extends State<CircularButton> {
+  Color darken(Color color, [double amount = .1]) {
+    assert(amount >= 0 && amount <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+    return hslDark.toColor();
+  }
+
+  Color buttonBackgroundColor = Colors.white;
+
+  @override
+  void initState() {
+    buttonBackgroundColor = widget.backgroundColor;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: widget.onTap,
+      onTapDown: (_) {
+        setState(() {
+          buttonBackgroundColor = darken(buttonBackgroundColor, 0.2);
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          buttonBackgroundColor = widget.backgroundColor;
+        });
+      },
       child: Container(
-        height: size,
-        width: size,
+        height: widget.size,
+        width: widget.size,
         decoration: BoxDecoration(
-          color: backgroundColor,
+          color: buttonBackgroundColor,
           shape: BoxShape.circle,
-          boxShadow: hasShadow ? kShadow : [],
+          boxShadow: widget.hasShadow ? kShadow : [],
         ),
         child: Icon(
-          icon,
-          size: iconSize,
+          widget.icon,
+          size: widget.iconSize,
         ),
       ),
     );
